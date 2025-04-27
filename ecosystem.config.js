70c443ceed851343d64cc00cfb394307af1b9ee7
Sefinek <contact@sefinek.net>
2025-04-27 06:53:04 +0200

    Update ecosystem.config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,29 +3,22 @@ module.exports = {
 		name: 'sefinek',
 		script: './index.js',
 
-		// Configuration options
-		exec_mode: 'fork',
-		// max_memory_restart: '1G',
-
-		// Monitoring changes in files and restarting the application
-		watch: false,
-		ignore_watch: ['.git', 'node_modules', 'logs', '.eslintrc.js', 'ecosystem.config.js'],
-
-		// Logging settings
+		// Logging configuration
 		log_date_format: 'HH:mm:ss.SSS DD.MM.YYYY',
 		merge_logs: true,
 		log_file: '/home/sefinek/logs/www/sefinek.net/combined.log',
 		out_file: '/home/sefinek/logs/www/sefinek.net/out.log',
 		error_file: '/home/sefinek/logs/www/sefinek.net/error.log',
 
-		// Application restart policy
+		// Application restart policy settings
 		wait_ready: true,
 		autorestart: true,
-		max_restarts: 5,
-		restart_delay: 4000,
-		min_uptime: 13000,
+		max_restarts: 10,
+		min_uptime: 20000,
+		restart_delay: 5000,
+		exp_backoff_restart_delay: 3000,
 
-		// Environment variables
+		// Environment variables configuration
 		env: {
 			NODE_ENV: 'production',
 		},
